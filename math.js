2d6ffddaa81350cffc9b0263eb3800a0d6760ead
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-04-22 11:45:33 -0300

    2.5

--- math.js
+++ math.js
@@ -1,6 +1,15 @@
 function pace()  {
     let distancia = document.getElementById("km1button");
     let tempo = document.getElementById("tempo1button");
-    document.getElementById("resultadokm").innerText = (distancia.value / (tempo.value*60))*3.6;
-    
+    let resultado = (distancia.value / (tempo.value*60))*3.6;
+    let resultadopace = (60/resultado);
+    document.getElementById("resultadokm").innerText = resultado.toFixed(2) +" KM/H";
+    document.getElementById("resultadopace").innerText = resultadopace.toFixed(2) +" MIN/H";
+}
+
+function calculartempo()   {
+    let metros = document.getElementById("metragem");
+    let horas = document.getElementById("pacepreciso");
+    let fica = (metros.value*horas.value)/1000;
+    document.getElementById("tempopreciso").innerText = fica+ " Minutos" ;
 }
