4c217e10809e6726bb0408794d4528c78b90f32f
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-04-23 14:07:56 -0300

    2.6

---
+++ contato.js
@@ -0,0 +1,7 @@
+function contato()   {
+    if (confirm('Deseja conversar no direct do Instagram?')) {
+        window.location.href = 'https://www.instagram.com/direct/t/100241694801195';
+      } else {
+        exit
+      }
+}
