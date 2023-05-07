da1538213cb14bfac3f91e53a11f119f7c12e0ae
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-05-07 11:11:11 -0300

    3.1
    rodape novo

--- contato.js
+++ contato.js
@@ -12,11 +12,17 @@ function galeria()    {
   window.location.href= "pictures.html";
 }
 function facaparte()    {
-  window.location.href= "mkt.html"
+  window.location.href= "mkt.html";
 }
 function calculadora()   {
   window.location.href= "math.html";
 }
 function home()    {
-  window.location.href= "index.html"
+  window.location.href= "index.html";
+}
+function instagram()   {
+  window.location.href= "https://www.instagram.com/grupodecorrida100limites/";
+}
+function facebook()    {
+  window.location.href= "https://www.facebook.com/grupodecorrida100limites"
 }
