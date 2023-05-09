c66a0a334aa0d693dd8870d0884f73b45c1a8a80
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-05-09 17:27:10 -0300

    3.2
    calculadora funcional;
    logo clikavel;

--- math.js
+++ math.js
@@ -15,3 +15,15 @@ function pace()  {
 	document.getElementById("resultadokm").innerText= paceMinutos + ":" + paceSegundos  + " Min/KM";    
 }
 
+function calculartempo()   {
+	let distancia= parseFloat(document.getElementById("metragem").value);
+	let pace= parseFloat(document.getElementById("pacepreciso").value);
+	let tempo= distancia*pace;
+	if(tempo >= 60){
+		tempo= tempo/60 + " Horas";
+	}else{
+		tempo= tempo + " Minutos";
+	}
+	
+	document.getElementById("tempopreciso").innerText= tempo;
+}
